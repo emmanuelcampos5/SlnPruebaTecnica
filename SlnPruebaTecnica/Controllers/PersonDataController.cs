43d2f33e78be3e590b08cc8ed6461beb00bc8a66
emmanuelcampos5 <94939174+emmanuelcampos5@users.noreply.github.com>
2025-02-18 19:19:38 -0600

    a

---
+++ SlnPruebaTecnica/Controllers/PersonDataController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SlnPruebaTecnica.Models;
+
+namespace SlnPruebaTecnica.Controllers
+{
+    public class PersonDataController : Controller
+    {
+        PruebaTecnicaDbContext db = new PruebaTecnicaDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Personas.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
